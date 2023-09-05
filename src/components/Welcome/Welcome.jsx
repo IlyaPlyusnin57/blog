5d1fa2341e687b33c3ee88c69e5c27e0d7a38890
Ilya Plyusnin <ilyaplyusnin57@gmail.com>
2023-09-04 22:10:24 -0400

    Create a Welcome component

---
+++ src/components/Welcome/Welcome.jsx
@@ -0,0 +1,11 @@
+import "./Welcome.scss";
+
+function Welcome() {
+  return (
+    <section className="welcome">
+      <h1>Welcome to my Blog</h1>
+    </section>
+  );
+}
+
+export default Welcome;

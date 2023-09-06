34223ae919738b9750ba81d386c5295837388558
Ilya Plyusnin <ilyaplyusnin57@gmail.com>
2023-09-05 23:53:35 -0400

    Add a link pointing to an article
    Also import a scss file

--- src/components/JavaScript/JavaScript.jsx
+++ src/components/JavaScript/JavaScript.jsx
@@ -1,7 +1,21 @@
+import { Link } from "react-router-dom";
+
+import "./JavaScript.scss";
+
 function JavaScript() {
   return (
     <section>
       <h1>JavaScript Articles</h1>
+
+      <div className="article-list">
+        <Link
+          className="article-link"
+          to="/javascript/remove-duplicates"
+          state={{ value: "RemoveDuplicates" }}
+        >
+          <span>Remove Duplicates from an Array</span>
+        </Link>
+      </div>
     </section>
   );
 }

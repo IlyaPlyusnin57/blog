c11bf78ce3ecbac4c67f7f786e6ec148f5a99e3b
Ilya Plyusnin <ilyaplyusnin57@gmail.com>
2023-09-06 21:17:10 -0400

    Add underline styling to the navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -5,13 +5,13 @@ import { Link } from "react-router-dom";
 function Navbar() {
   return (
     <nav>
-      <Link to="/">
+      <Link to="/" className="nav-link">
         <span>Home</span>
       </Link>
-      <Link to="/dsa">
+      <Link to="/dsa" className="nav-link">
         <span>DSA</span>
       </Link>
-      <Link to="/javascript">
+      <Link to="/javascript" className="nav-link">
         <span>JavaScript</span>
       </Link>
     </nav>

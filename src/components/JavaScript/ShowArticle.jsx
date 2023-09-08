5717ccc5e406bed9b80ec1d0a8206897ba64e4d8
Ilya Plyusnin <ilyaplyusnin57@gmail.com>
2023-09-07 20:34:37 -0400

    Add IsArray to mappingComponents

--- src/components/JavaScript/ShowArticle.jsx
+++ src/components/JavaScript/ShowArticle.jsx
@@ -1,9 +1,12 @@
 import { useLocation } from "react-router-dom";
 import RemoveDuplicates from "./Articles/RemoveDuplicates";
+import IsArray from "./Articles/IsArray";
+
 import "./ShowArticle.scss";
 
 const mappingComponents = {
   RemoveDuplicates,
+  IsArray,
 };
 
 function ShowArticle() {

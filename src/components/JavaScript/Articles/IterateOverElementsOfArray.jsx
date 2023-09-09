3520816eb5493f40f5fa28b7528618bc481ce24d
Ilya Plyusnin <ilyaplyusnin57@gmail.com>
2023-09-08 21:26:13 -0400

    Add first solution

--- src/components/JavaScript/Articles/IterateOverElementsOfArray.jsx
+++ src/components/JavaScript/Articles/IterateOverElementsOfArray.jsx
@@ -1,5 +1,23 @@
 function IterateOverElementsOfArray() {
-  return <div>IterateOverElementsofArray</div>;
+  return (
+    <section className="article-container">
+      <h1>Iterate Over All Elements of an Array</h1>
+
+      <h2>Goal</h2>
+
+      <p>How to iterate over all elements in an Array</p>
+
+      <h2>Solution</h2>
+
+      <p>1. Use for of loop</p>
+
+      <code>
+        <div className="mb-1">const arr = [1,2,3,4];</div>
+        for (const number of arr) &#123; <br />
+        <span className="indent">console.log(number);</span> <br /> &#125;
+      </code>
+    </section>
+  );
 }
 
 export default IterateOverElementsOfArray;
